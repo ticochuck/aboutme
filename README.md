ef6fdfae3cfc8c1d18b41e9e01d434d078c4c559
ticochuck <chucklivilla@gmail.com>
2020-01-18 17:51:49 -0800

    lab03

--- README.md
+++ README.md
@@ -4,15 +4,25 @@ This page contains a short biography about myself, my education history, and pro
 
 
 # Deployment steps
-I started by creating the aboutme repo in Github. Then I created all the files and directories in the terminal. I started adding HTML, CSS, and JS content utilizing what we have leaned in class so far. 
 
-In the index.html file, I am using a \<p> element to display my short bio. 
+## Lab 02 
+>I started by creating the aboutme repo in Github. Then I created all the files and directories in the terminal. I started adding HTML, CSS, and JS content utilizing what we have leaned in class so far. 
 
-In the style.css file I am styling the html content using different elements and attributes. 
+>In the index.html file, I am using a \<p> element to display my short bio. 
 
-In the apps.js file, I welcome the user with a prompt asking for their name. Then I ask 5 yes or not questions and retrnung a message letting them know if they answered correctly or not. 
+>In the style.css file I am styling the html content using different elements and attributes. 
 
-I have been making constant changes to these files and I am following the Add, Commit, Push process to keep the latest version in github.
+>In the apps.js file, I welcome the user with a prompt asking for their name. Then I ask 5 yes or not questions and retrnung a message letting them know if they answered correctly or not. 
+
+>I have been making constant changes to these files and I am following the Add, Commit, Push process to keep the latest version in github.
+
+## Lab 03
+> 
+
+# Challanges
+
+- Adding a link to 2 images next to each other kept showing an undersocre (\_) in between the images. I had to make sure I had the \<a> and \<img> elements in the same line of code to remove the (\_).
+
 
 # Author 
 - Chuck Li Villalobos
--- js/apps.js
+++ js/apps.js
@@ -6,14 +6,17 @@ alert('Hello. Welcome to the About Me page!');
 //get usernmae
 var userName = prompt(' Before we get started, please tell us your name :');
 
-//final message
-alert('Hi ' + userName + '! \nMy name is Chuck. So that you can get to know me a little better, I am going to ask you 5 (yes or no) questions about myself. \nTry guessing all the correct answers! Good luck!');
+// intro message
+alert('Hi ' + userName + '! \nMy name is Chuck. So that you can get to know me a little better, I am going to ask you a few questions about myself. \nTry guessing all the correct answers! There will be a score at the end. Good luck!');
+
+var correctAnswers = 0;
 
 //Question #1
 var question1 = prompt('Question #1. \nAm I from Puerto Rico?');
 if ((question1.toLowerCase() === 'no') || (question1.toLowerCase() === 'n')) {
   //console.log('CORRECT! \nI was born and raised in Costa Rica!');
   alert('CORRECT! \nI was born and raised in Costa Rica!');
+  correctAnswers ++;
 } else if ((question1.toLowerCase() === 'yes') || (question1.toLowerCase() === 'y')) {
   //console.log('Incorrect :( \nClose but not really. I was born and raised in Costa Rica.');
   alert('Incorrect :( \nClose but not really. I was born and raised in Costa Rica.');
@@ -23,6 +26,7 @@ var question2 = prompt('Question #2. \nAm I chinese?');
 if ((question2.toLowerCase() === 'yes') || (question2.toLowerCase() === 'y')) {
   //console.log('CORRECT! \nMy dad is from China and my mom is from Costa Rica. That makes me a rare Costarican-Chinese!');
   alert('CORRECT! \nMy dad is from China and my mom is from Costa Rica. That makes me a rare Costarican-Chinese!');
+  correctAnswers ++;
 } else if ((question2.toLowerCase() === 'no') || (question2.toLowerCase() === 'n')) {
   //console.log('Incorrect :( \nEven though I was born in Costa Rica, my dad is from China, so that makes me a rare Costarican-Chinese!');
   alert('Incorrect :( \nEven though I was born in Costa Rica, my dad is from China, so that makes me a rare Costarican-Chinese!');
@@ -33,6 +37,7 @@ var question3 = prompt('Question #3. \nDo I like burgers?');
 if ((question3.toLowerCase() === 'yes') || (question3.toLowerCase() === 'y')) {
   //console.log('CORRECT! \nI love burgers. I can eat one every day!!');
   alert('CORRECT! \nI love burgers. I can eat one every day!!');
+  correctAnswers ++;
 } else if ((question3.toLowerCase() === 'no') || (question3.toLowerCase() === 'n')) {
   //console.log('Incorrect :( \nWho doesn\'t love burgers?? I love burgers, well done and no onions please!');
   alert('Incorrect :( \nWho doesn\'t love burgers?? I love burgers, well done and no onions please!');
@@ -40,9 +45,10 @@ if ((question3.toLowerCase() === 'yes') || (question3.toLowerCase() === 'y')) {
 
 //Question #4
 var question4 = prompt('Question #4. \nDo I have 4 kids under 6 years old?');
-if ((question4.toLowerCase() === 'no') || (question2.toLowerCase() === 'n')) {
+if ((question4.toLowerCase() === 'no') || (question4.toLowerCase() === 'n')) {
   //console.log('CORRECT! \nI ONLY have 3 kids under 6 years old');
   alert('CORRECT! \nI ONLY have 3 kids under 6 years old');
+  correctAnswers ++;
 } else if ((question4.toLowerCase() === 'yes') || (question4.toLowerCase() === 'y')) {
   //console.log('Incorrect :( \nWhat are you thinking, ' + userName + '? That would be crazy... I ONLY have 3 kids 6 and under...');
   alert('Incorrect :( \nWhat are you thinking, ' + userName + '? That would be crazy... I ONLY have 3 kids 6 and under...');
@@ -53,11 +59,64 @@ var question5 = prompt('Question #5. \nAm I married?');
 if ((question5.toLowerCase() === 'yes') || (question5.toLowerCase() === 'y')) {
   //console.log('CORRECT! \nAnd actually, this year me and my wife Laura are celebrating our 10th anniversary');
   alert('CORRECT! \nAnd actually, this year me and my wife Laura are celebrating our 10th anniversary');
+  correctAnswers ++;
 } else if ((question5.toLowerCase() === 'no') || (question5.toLowerCase() === 'n')) {
   //console.log('Incorrect :( \nMy wife Laura and I have been married for 10 years.');
   alert('Incorrect :( \nMy wife Laura and I have been married for 10 years.');
 }
 
-alert('Thank you for playing ' + userName + '! See next time.');
+//Question #6
+var myGrandmaAge = '91';
 
+//give user 4 chances to get the correct answer
+for (var i = 1; i < 5; i++) {
+var question6 = prompt('How old my grandma?');
+  if (question6 === myGrandmaAge) {
+    correctAnswers ++;
+    if (i === 1) {
+      alert('Awesome you guessed it on your ' + i + 'st try');
+    } else if (i === 2) {
+      alert('Awesome you guessed it on your ' + i + 'nd try');
+    } else {
+      alert('Awesome you guessed it on your ' + i + 'th try');
+    }
+      break;  
+    } else if (question6 > myGrandmaAge) {
+      alert('too high. Try again')
+  } else if (question6 < myGrandmaAge) {
+    alert('too low. Try again');
+  }
+}
+//if user does not guess in 4 attemps
+if (i > 4 ) { 
+  alert('Sorry, you ran out of chances. My grandma is ' + myGrandmaAge + ' years old.');
+}
+
+//Question #7
+var favoriteRestaurant = ['red robin', 'red mill', 'dicks']
+console.log('favoriteRestaurant array = ' + favoriteRestaurant);
+
+//first loop is for 6 attemps to get the correct answer
+for (var j = 0; j < 6; j++) {
+  var question7 = prompt('What is my favorite burger restaurant?');
+  //this second loop is to go through the entire array 
+  for (var x = 0; x < favoriteRestaurant.length; x++) {
+  console.log('this is faverestaurant[x] = ' + favoriteRestaurant[x]);
+    if (question7.toLowerCase() === favoriteRestaurant[x]) {
+      alert('YES, ' + favoriteRestaurant + ' are some of my favorite burger restaurants');
+      correctAnswers ++;
+      var alreadyGuessed = true; //if guessed, exit the loop 
+      break;
+    }
+  } 
+  if (alreadyGuessed === true) {
+    j = 6;
+  } else if (j < 5) {
+    alert('Nope. try again');
+  } else if (j === 5) {
+      alert('Sorry, you run out of chances');
+  }
+}
+
+alert('Thank you for playing ' + userName + '! You got ' + correctAnswers + ' correct answers out of 7. See next time!');
 
